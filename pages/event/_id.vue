cbb1b285b92103b74092d6d1c813642d4b83fefe
Evgeny <evgenygost@gmail.com>
2020-05-20 19:04:58 +0400

    Async await axios + progress bar

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
-    <h1>Event #{{ id }}</h1>
+    <h1>{{ event.title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    id() {
-      return this.$route.params.id
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get(
+        'http://localhost:3030/events/' + params.id
+      )
+      return {
+        event: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: `Unable to fetch event #${params.id} at this time. Please try again.`
+      })
     }
   },
   head() {
     return {
-      title: 'Event #' + this.id,
+      title: this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'My description for event #' + this.id
+          content: 'My description for ' + this.event.title
         }
       ]
     }
